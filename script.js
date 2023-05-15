c4050383c37aa007112780323c977cb86b5e62ac
lewisthagichu <lewisthagichu254@gmail.com>
2023-05-15 17:30:34 +0300

    Implement new wildwest theme

--- script.js
+++ script.js
@@ -1,71 +1,80 @@
-// Initialize player and computer scores
+const options = document.querySelectorAll(".options");
 let playerScore = 0;
 let computerScore = 0;
 
-// Function to generate computer's choice
-function getComputerChoice() {
-  const randomGuess = Math.floor(Math.random() * 1000);
+options.forEach((option) => {
+  option.addEventListener("click", function () {
+    const pInput = this.value;
 
-  // Determine computer's choice based on the random number
-  if (randomGuess % 3 === 0){
-    return "rock";
-  } else if (randomGuess % 3 === 1){
-    return "scissors";
-  } else {
-    return "paper";
+    const cOptions = ["Rock", "Paper", "Scissors"];
+    const cInput = cOptions[Math.floor(Math.random() * 3)];
+
+    updateMove(pInput, cInput);
+    compareInputs(pInput, cInput);
+    updateScore();
+    if (checkWinner()) {
+      playerScore = computerScore = 0;
+      updateScore();
+    }
+  });
+});
+
+function compareInputs(pInput, cInput) {
+  const currentMatch = `${pInput} vs ${cInput}`;
+  if (pInput === cInput) {
+    alert(`${currentMatch} is a Tie`);
+    return;
+  }
+
+  if (pInput === "Rock") {
+    if (cInput === "Scissors") {
+      alert(`${currentMatch} = You Win`);
+      playerScore++;
+    } else {
+      alert(`${currentMatch} = Computer Wins`);
+      computerScore++;
+    }
+  }
+  //Check for Paper
+  else if (pInput === "Paper") {
+    if (cInput === "Rock") {
+      alert(`${currentMatch} = You Win`);
+      playerScore++;
+    } else {
+      alert(`${currentMatch} = Computer Wins`);
+      computerScore++;
+    }
+  }
+  //Check for Scissors
+  else {
+    if (cInput === "Paper") {
+      alert(`${currentMatch} = You Win`);
+      playerScore++;
+    } else {
+      alert(`${currentMatch} = Computer Wins`);
+      computerScore++;
+    }
   }
 }
 
-// Function to play a round of the game
-function playRound(playerSelection, computerSelection) {
-  // Check for a tie
-  if (playerSelection === computerSelection) {
-    playerScore = playerScore;
-    computerScore = computerScore;
-    return "Tie! Try again";
-  } 
-  // Check for player wins
-  else if (playerSelection == "rock" && computerSelection == "scissors") {
-    playerScore++;
-    return "You win! Rock beats Scissors";
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    playerScore++;
-    return "You win! Scissors beat Paper";
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
-    playerScore++;
-    return "You win! Paper beats Rock";
-  } 
-  // Check for computer wins
-  else if (playerSelection == "scissors" && computerSelection == "rock") {
-    computerScore++;
-    return "You lose! Rock beats Scissors";
-  } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    computerScore++;
-    return "You lose! Scissors beats Paper";
-  } else if (playerSelection == "rock" && computerSelection == "paper") {
-    computerScore++;
-    return "You lose! Paper beats Rock";
-  } 
+function updateScore() {
+  document.getElementById("playerScore").textContent = playerScore;
+  document.getElementById("computerScore").textContent = computerScore;
 }
 
-// Play the game
-while (computerScore < 5 && playerScore < 5) {
-  // Get user's choice and computer's choice
-  let playerSelection = prompt("Rock, Paper or Scissors: ").toLowerCase();
-  let computerSelection = getComputerChoice();
-
-  // Play a round and log the result and scores
-  console.log(playRound(playerSelection, computerSelection));
-  console.log("Computer score:", computerScore);
-  console.log("Player score:", playerScore);
+function checkWinner() {
+  if (playerScore === 5 || computerScore === 5) {
+    const winner =
+      playerScore === 5
+        ? "You win the game! Congratulations!"
+        : "Computer wins the game! Try again next time!";
+    alert(winner);
+    return true;
+  }
+  return false;
 }
 
-// Declare the winner
-if (playerScore == 5) {
-    console.log("Congratulations! You win!");
-} else if (computerScore == 5) {
-    console.log("Sorry, you lost :(");
+function updateMove (pInput, cInput) {
+  document.getElementById("p-move").src = `./img/${pInput}.png`;
+  document.getElementById("c-move").src = `./img/${cInput}.png`;
 }
-
-
-
