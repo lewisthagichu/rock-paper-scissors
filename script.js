daff1e8a0933e477d8f333a49ac35963176ea4b7
lewisthagichu <lewisthagichu254@gmail.com>
2023-06-01 18:46:25 +0300

    Initial commit

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const options = document.querySelectorAll(".options");
 let playerScore = 0;
 let computerScore = 0;
 
+let resultContainer = document.querySelector('.results-container');
+let restartBtn = document.querySelector('.restart-btn');
+
+
 options.forEach((option) => {
   option.addEventListener("click", function () {
     const pInput = this.value;
@@ -9,12 +13,10 @@ options.forEach((option) => {
     const cOptions = ["Rock", "Paper", "Scissors"];
     const cInput = cOptions[Math.floor(Math.random() * 3)];
 
-    updateMove(pInput, cInput);
     compareInputs(pInput, cInput);
     updateScore();
     if (checkWinner()) {
-      playerScore = computerScore = 0;
-      updateScore();
+      declareWinner();
     }
   });
 });
@@ -22,39 +24,51 @@ options.forEach((option) => {
 function compareInputs(pInput, cInput) {
   const currentMatch = `${pInput} vs ${cInput}`;
   if (pInput === cInput) {
-    alert(`${currentMatch} is a Tie`);
+    displayResults(`It's a tie! Both you and the computer chose ${pInput}`);
     return;
   }
 
   if (pInput === "Rock") {
     if (cInput === "Scissors") {
-      alert(`${currentMatch} = You Win`);
+      displayResults("Nice move! You won. Rock smashes scissors.");
       playerScore++;
     } else {
-      alert(`${currentMatch} = Computer Wins`);
+      displayResults("Hard luck! The computer won. Paper smothers your rock.");
       computerScore++;
     }
   }
   //Check for Paper
   else if (pInput === "Paper") {
     if (cInput === "Rock") {
-      alert(`${currentMatch} = You Win`);
+      displayResults("Victory is paper-thin! Rock couldn't tear through paper.");
       playerScore++;
     } else {
-      alert(`${currentMatch} = Computer Wins`);
+      displayResults("Bummer! The computer won. Your paper got snipped by scissors.");
       computerScore++;
     }
   }
   //Check for Scissors
-  else {
+  else if (pInput === "Scissors") {
     if (cInput === "Paper") {
-      alert(`${currentMatch} = You Win`);
+      displayResults("Sharp choice! You won. Scissors cut through paper.");
       playerScore++;
     } else {
-      alert(`${currentMatch} = Computer Wins`);
+      displayResults("Rock reigns supreme! The computer won. Your scissors couldn't break through.");
       computerScore++;
     }
   }
+}
+
+// function to display results after each round
+function displayResults(str) {
+  resultContainer.animate([{ opacity: 0 }, { opacity: 1 }], {
+    duration: 300,
+    fill: "forwards",
+    iterations: 1,
+    delay: 0,
+    easing: "ease-out",
+  });
+  resultContainer.textContent = str;
 }
 
 function updateScore() {
@@ -64,17 +78,26 @@ function updateScore() {
 
 function checkWinner() {
   if (playerScore === 5 || computerScore === 5) {
-    const winner =
-      playerScore === 5
-        ? "You win the game! Congratulations!"
-        : "Computer wins the game! Try again next time!";
-    alert(winner);
+    resultContainer.style.fontSize = "25px";
     return true;
   }
   return false;
 }
 
-function updateMove (pInput, cInput) {
-  document.getElementById("p-move").src = `./img/${pInput}.png`;
-  document.getElementById("c-move").src = `./img/${cInput}.png`;
+// function to declare winner
+function declareWinner() {
+  if (playerScore > computerScore) {
+    resultContainer.textContent = "Victory is yours! With your strategic choices, you have proven to be the true master of rock, paper, scissors."
+  } else if (playerScore < computerScore) {
+    resultContainer.textContent = "A tough defeat! Despite your best efforts, the computer's mastery of rock, paper, scissors has proven too formidable to overcome."
+  }
+
+  restartBtn.style.display = "inline-block";
+  restartBtn.addEventListener("click", function() {
+    playerScore = computerScore = 0;
+    updateScore();
+    restartBtn.style.display = "none";
+    resultContainer.textContent = '';
+    resultContainer.style.fontSize = "20px";
+  })
 }
